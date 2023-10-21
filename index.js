b5ab79f42ed0d1ba1c3969142efe8f19901ceccc
alexmerder <63518412+AlexMerder@users.noreply.github.com>
2023-10-21 09:18:28 +0700

    added score, price, fine, result, question number, update difficulties

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { bot } = require('./src/bot/bot.js');
-const { UserSession } = require('./src/models/user_session.js');
 const { newGame, rulesAndDonation, modeList, difficultyModes, back, payRespect, placeholder } = require('./src/bot/opts.js');
-const { generatedQuestion } = require('./src/bot/commands.js');
 const { rules, selectDifficulty, ACTIONS } = require('./src/bot/text.js');
+const { setDifficulty, findOrCreateUser, setTopicAndSave } = require('./src/bot/services/userService.js')
+const { inDevelopment, sendGeneratedQuestion, sendMessage } = require('./src/bot/commands.js')
 
 bot.onText(/\/start/, async (msg) => {
 
@@ -15,13 +15,12 @@ bot.onText(/\/start/, async (msg) => {
     } catch (error) {
         console.log('Error: ' + error);
     }
-
 });
 
 bot.on('callback_query', async (callbackQuery) => {
 
     try {
-        console.log("UserName: ", callbackQuery.from.username);
+        console.log("username: ", callbackQuery.from.username);
         console.log("message: ", callbackQuery.message.text);
 
 
@@ -30,25 +29,29 @@ bot.on('callback_query', async (callbackQuery) => {
         const action = callbackQuery.data;
 
         let session = await findOrCreateUser(chatId, userName);
-        // console.log('User session: ', session);
 
         switch (action) {
             case ACTIONS.NEW_GAME:
             case ACTIONS.BACK:
+                resetData(session);
+                console.log('Button clicked: ', action);
                 await sendMessage(chatId, `*${userName}*, rules are simple - answer on questions right!`, rulesAndDonation);
                 break;
 
             case ACTIONS.GET_STARTED:
+                console.log('Button clicked: ', action);
                 await sendMessage(chatId, 'Pick a game mode: ', modeList);
                 break;
 
             case ACTIONS.PLACEHOLDER:
             case ACTIONS.PAY_RESPECT:
             case ACTIONS.ACHIEVEMENTS:
+                console.log('Button clicked: ', action);
                 await inDevelopment(chatId);
                 break;
 
             case ACTIONS.RULES:
+                console.log('Button clicked: ', action);
                 await sendMessage(chatId, rules, back);
                 break;
 
@@ -56,30 +59,45 @@ bot.on('callback_query', async (callbackQuery) => {
             case ACTIONS.SUBTRACTION:
             case ACTIONS.MULTIPLICATION:
             case ACTIONS.RANDOM:
+                console.log('Button clicked: ', action);
                 await setTopicAndSave(session, action);
+                console.log('Topic set: ', action);
                 await sendMessage(chatId, selectDifficulty, difficultyModes);
                 break;
 
             case ACTIONS.EASY:
             case ACTIONS.NORMAL:
             case ACTIONS.HARDCORE:
+                console.log('Button clicked: ', action);
                 await setDifficulty(session, action);
+                console.log('Topic set: ', action);
                 try {
                     if (!session.topic || !session.difficulty) {
                         await sendMessage(chatId, `Welcome 👋*${userName}* to the game "*Who wants to be smart?*"`, newGame);
                     } else {
+                        session.questionNumber = 1;
+                        session.score = 0;
                         await sendGeneratedQuestion(session, chatId);
+                        console.log('Question sent');
                     }
                 } catch (error) {
-                    
+                    console.log('Error: ', error);
                 }
-                
                 break;
 
             case ACTIONS.CORRECT_ANSWER:
                 sendMessage(chatId, `Correct!`, {})
                     .then(() => {
-                        return sendGeneratedQuestion(session, chatId);
+                        if (session.answered) {
+                            return;
+                        }
+                        clearTimeout(session.timerId);
+                        session.answered = true;
+                        session.questionNumber++;
+                        session.score += session.price;
+                        questionTimeout = setTimeout(() => {
+                            sendGeneratedQuestion(session, chatId);
+                        }, 1500)
                     })
                     .catch(error => {
                         console.log('Error: ', error);
@@ -88,7 +106,16 @@ bot.on('callback_query', async (callbackQuery) => {
             case ACTIONS.INCORRECT_ANSWER:
                 sendMessage(chatId, `Incorrect!`, {})
                     .then(() => {
-                        return sendGeneratedQuestion(session, chatId);
+                        if (session.answered) {
+                            return;
+                        }
+                        clearTimeout(session.timerId);
+                        session.answered = true;
+                        session.questionNumber++;
+                        session.score -= session.fine;
+                        questionTimeout = setTimeout(() => {
+                            sendGeneratedQuestion(session, chatId);
+                        }, 1500)
                     })
                     .catch(error => {
                         console.log('Error: ', error);
@@ -102,85 +129,7 @@ bot.on('callback_query', async (callbackQuery) => {
     }
 });
 
-async function setDifficulty(session, difficulty) {
-    try {
-        session.difficulty = difficulty;
-        await session.save();
-        console.log('Difficulty has been set: ' + difficulty);
-    } catch (error) {
-        console.log('Error in db: ', error);
-        await sendMessage(chatId, `Welcome 👋*${userName}* to the game "*Who wants to be smart?*"`, newGame);
-    }
-}
-
-async function setTopicAndSave(session, topic) {
-    try {
-        session.topic =  topic;
-        await session.save();
-        console.log('Topic has been set: ' + topic);
-    } catch (error) {
-        console.log('Error in db: ', error);
-        await sendMessage(chatId, `Welcome 👋*${userName}* to the game "*Who wants to be smart?*"`, newGame);
-    }
-}
-
-async function findOrCreateUser(chatId, userName) {
-    try {
-        let session = await UserSession.findOne({ where: { chatId: chatId } });
-        if (!session) {
-            session = await UserSession.create({ chatId, userName });
-            return session;
-        }
-        await session.save();
-        return session;
-    } catch (error) {
-        console.log('Error in db: ', error);
-        await sendMessage(chatId, `Welcome 👋*${userName}* to the game "*Who wants to be smart?*"`, newGame);
-    }
-}
-
-async function sendMessage(chatId, message, options) {
-    try {
-        return bot.sendMessage(chatId, message, options);
-
-    } catch (error) {
-        console.log('Error while sending message via telegram: ', error);
-    }
-}
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-async function sendGeneratedQuestion(session, chatId) {
-    const questionData = await generatedQuestion(session.topic, session.difficulty);
-    const { question, correctAnswer, uniqueIncorrectAnswers } = questionData;
-
-    const allAnswers = [
-        { answer: correctAnswer, callback_data: 'correct_answer' },
-        ...uniqueIncorrectAnswers.map(answer => ({ answer, callback_data: 'incorrect_answer' }))
-    ];
-
-    const shuffledAnswers = shuffleArray([...allAnswers]);
-
-    console.log("shuffledAnswers: " + shuffledAnswers);
-    const gameQuestion = {
-        reply_markup: {
-            inline_keyboard: [
-                ...shuffledAnswers.map(answerObj => [
-                    { text: answerObj.answer.toString(), callback_data: answerObj.callback_data }
-                ]),
-                [{ text: "Finish", callback_data: 'newGame' }]
-            ]
-        }
-    };
-    return sendMessage(chatId, question, gameQuestion);
-}
-
-async function inDevelopment(chatId) {
-    return sendMessage(chatId, 'In development...', back);
+function resetData(session) {
+    session.questionNumber = null;
+    clearTimeout(session.timerId);
 }
--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,43 +1,51 @@
-const { difficultyModes } = require("./opts");
-const { ACTIONS } = require('./text');
+const { bot } = require('./bot');
+const { difficultyModes, back } = require('./opts.js');
+const { ACTIONS } = require('./text.js');
 
-const generatedQuestion = async (topic, difficulty) => {
-
+const generatedQuestion = async (topic, difficulty, questionNumber, score) => {
     let dif;
-
     switch (difficulty) {
         case ACTIONS.EASY:
-            dif = 31;
+            dif = 9;
             break;
         case ACTIONS.NORMAL:
-            dif = 101;
+            dif = 100;
             break;
         case ACTIONS.HARDCORE:
-            dif = 1001;
+            dif = 999;
             break;
         default:
-            dif = 101;
+            dif = 100;
     }
 
     let number1 = Math.floor(Math.random() * dif);
     let number2 = Math.floor(Math.random() * dif);
 
+    while (number1 === 0) {
+        number1 = Math.floor(Math.random() * dif);
+    }
+    while (number2 === 0) {
+        number2 = Math.floor(Math.random() * dif);
+    }
+
     let question = '';
     let correctAnswer = 0;
 
+    let { price, fine } = getPriceAndFine(questionNumber);
+
     switch (topic) {
         case ACTIONS.ADDITION:
-            question = `What is ${number1} + ${number2} ?`;
+            question = `Score: ${score}\nQuestion ${questionNumber}:\nPrice: ${price}\nWhat is ${number1} + ${number2} ?`;
             correctAnswer = number1 + number2;
             break;
 
         case ACTIONS.SUBTRACTION:
-            question = `What is ${number1} - ${number2} ?`;
+            question = `Score: ${score}\nQuestion ${questionNumber}:\nPrice: ${price}\nWhat is ${number1} - ${number2} ?`;
             correctAnswer = number1 - number2;
             break;
 
         case ACTIONS.MULTIPLICATION:
-            question = `What is ${number1} * ${number2} ?`;
+            question = `Score: ${score}\nQuestion ${questionNumber}:\nPrice: ${price}\nWhat is ${number1} * ${number2} ?`;
             correctAnswer = number1 * number2;
             break;
 
@@ -49,8 +57,8 @@ const generatedQuestion = async (topic, difficulty) => {
             }
             return generatedQuestion(randomOp, difficulty);
         default:
-            console.error('topic: ' + topic);
-            console.error('difficultyModes: ' + difficultyModes);
+            console.error('topic: ', topic);
+            console.error('difficultyModes: ', difficultyModes);
     }
 
     let uniqueIncorrectAnswers = [];
@@ -59,9 +67,26 @@ const generatedQuestion = async (topic, difficulty) => {
         uniqueIncorrectAnswers.push(generateIncorrectUniqueAnswer(correctAnswer, uniqueIncorrectAnswers, difficulty));
     }
 
-    return { question, correctAnswer, uniqueIncorrectAnswers };
+    return { question, correctAnswer, uniqueIncorrectAnswers, price, fine };
 }
 
+function getPriceAndFine(questionNumber) {
+    let priceValue, fineValue;
+    if (questionNumber >= 1 && questionNumber <= 5) {
+        priceValue = 100;
+        fineValue = 50;
+    } else if (questionNumber >= 6 && questionNumber <= 10) {
+        priceValue = 150;
+        fineValue = 150;
+    } else if (questionNumber >= 11 && questionNumber <= 15) {
+        priceValue = 200;
+        fineValue = 250;
+    } else {
+        priceValue = 100;
+        fineValue = 50;
+    }
+    return { price: priceValue, fine: fineValue };
+}
 
 function generateIncorrectUniqueAnswer(correctAnswer, existingAnswers, difficulty) {
     let uniqueAnswer;
@@ -85,8 +110,90 @@ function generateIncorrectUniqueAnswer(correctAnswer, existingAnswers, difficult
                 uniqueAnswer = correctAnswer + offset;
         }
     } while (existingAnswers.includes(uniqueAnswer) ||
-    uniqueAnswer === correctAnswer);
+        uniqueAnswer === correctAnswer);
     return uniqueAnswer;
 }
 
-module.exports = { generatedQuestion };
+async function sendMessage(chatId, message, options) {
+    try {
+        return bot.sendMessage(chatId, message, options);
+
+    } catch (error) {
+        console.log('Error while sending message via telegram: ', error);
+    }
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function getTimeout(difficulty) {
+    switch (difficulty) {
+        case ACTIONS.EASY:
+            return 10000;
+        case ACTIONS.NORMAL:
+            return 30000;
+        case ACTIONS.HARDCORE:
+            return 60000;
+        default:
+            return 30000;
+    }
+}
+
+async function sendGeneratedQuestion(session, chatId) {
+
+    if (session.questionNumber > 15) {
+        return sendMessage(chatId, `Your result: ${session.score}`, back);
+    }
+
+    const questionData = await generatedQuestion(session.topic, session.difficulty, session.questionNumber, session.score);
+    const { question, correctAnswer, uniqueIncorrectAnswers, price, fine } = questionData;
+
+    session.timerId = null;
+    session.answered = false;
+    session.price = price;
+    session.fine = fine;
+
+    const allAnswers = [
+        { answer: correctAnswer, callback_data: 'correct_answer' },
+        ...uniqueIncorrectAnswers.map(answer => ({ answer, callback_data: 'incorrect_answer' }))
+    ];
+
+    const shuffledAnswers = shuffleArray([...allAnswers]);
+
+    console.log("Shuffled answers for question 🔽: ", shuffledAnswers);
+    const gameQuestion = {
+        reply_markup: {
+            inline_keyboard: [
+                ...Array(Math.ceil(shuffledAnswers.length / 2)).fill().map((_, index) => {
+                    const offset = index * 2;
+                    return [
+                        { text: shuffledAnswers[offset].answer.toString(), callback_data: shuffledAnswers[offset].callback_data },
+                        ...(shuffledAnswers[offset + 1] ? [{ text: shuffledAnswers[offset + 1].answer.toString(), callback_data: shuffledAnswers[offset + 1].callback_data }] : [])
+                    ];
+                }),
+                [{ text: "Finish", callback_data: 'new_game' }]
+            ]
+        }
+    };
+    await sendMessage(chatId, question, gameQuestion);
+    const timeLimit = getTimeout(session.difficulty);
+    session.timerId = setTimeout(() => {
+        if (session.answered) {
+            return;
+        }
+        session.answered = true;
+        sendMessage(chatId, 'Time expired!', back);
+        console.log(`Time expired: ${session.userName}`);
+    }, timeLimit);
+}
+
+async function inDevelopment(chatId) {
+    return sendMessage(chatId, 'In development...', back);
+}
+
+module.exports = { inDevelopment, sendGeneratedQuestion, sendMessage };
